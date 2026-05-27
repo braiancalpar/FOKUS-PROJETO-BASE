279aa06fcd57d990c4a2f178066cd54578c3ffeb
braiancalpar <braian.santos@calpar.com.br>
2026-05-27 17:50:38 -0300

    Andamento da aula

--- script-crud.js
+++ script-crud.js
@@ -5,8 +5,13 @@ const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
 const ulTarefas = document.querySelector(".app__section-task-list");
 const btnCancelar = document.querySelector(".app__form-footer__button--cancel");
+const paragrafoDescricaoTarefa = document.querySelector(
+  ".app__section-active-task-description",
+);
 
-const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 const limparFormulario = () => {
   textarea.value = "";
@@ -53,6 +58,30 @@ function criarElementoTarefa(tarefa) {
   li.append(svg);
   li.append(paragrafo);
   li.append(botao);
+
+  if (tarefa.completa) {
+    li.classList.add("app__section-task-list-item-complete");
+    botao.setAttribute("disabled", "disabled");
+  } else {
+    li.onclick = () => {
+      document
+        .querySelectorAll(".app__section-task-list-item-active")
+        .forEach((elemento) => {
+          elemento.classList.remove("app__section-task-list-item-active");
+        });
+      if (tarefaSelecionada == tarefa) {
+        paragrafoDescricaoTarefa.textContent = "";
+        tarefaSelecionada = null;
+        liTarefaSelecionada = null;
+        return;
+      }
+      tarefaSelecionada = tarefa;
+      liTarefaSelecionada = li;
+      paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+
+      li.classList.add("app__section-task-list-item-active");
+    };
+  }
 
   return li;
 }
